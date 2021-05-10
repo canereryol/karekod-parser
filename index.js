c180a4d7b179aeb8f2ce7c4b8a4de511b58538de
Caner Eryol <caner.eryol@teb.org.tr>
2021-05-10 16:26:22 +0300

    closes #2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var moment = require('moment');
 
 exports.parse = function (karekod) {
+  if (!karekod) {
+    return null
+  }
   var karekodExp = /010(\d{13})21([^\W_]{1,20})-?17(\d{2})(0[1-9]|1[012])(00|0[1-9]|[12][0-9]|3[01])10([^\W_]{1,20})$/g;
-  var kk = karekodExp.exec(karekod);
+  var kk = karekod.match(karekodExp);
   if (!kk) {
     return null;
   } else {
